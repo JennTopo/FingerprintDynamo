276ee5fe414b592c8cd8b3e8270e716c2b85b0b0
JennTopo <127803618+JennTopo@users.noreply.github.com>
2023-04-04 11:41:30 -0400

    added new notes
    added new notes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 /*Have to make some arrays in order for the button to know what how long or short the password length is 
-as well as the possible choices in letters, digits and speical characters. */
+as well as the possible choices in letters, digits and special characters. */
 
-var passwordlenght = (i=0)[i>8 & i < 128];
-var characterlength = 8;
+var passwordlength = (i=0)[i>8 & i < 128];
+var characterlength= 8;
 var choice= [];
 var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t', 'u','v','w','x','y','z'];
 var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W', 'X','Y','Z'];
@@ -11,7 +11,6 @@ var speical = ['!','@','#','$','%','&','*','(',')','-','_','+','=','{','}','[','
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate"); // in here is where the HTML button coding is linked to
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); //when this function takes places it jumps down to the next line of coding.
@@ -34,10 +33,11 @@ else {
 
 //this statement is now pulling data from the var arrays listed in the top of the js page
 // the i++ means you cna keep trying different possiblities
+//this is where the value of the password is being stored
 generatePassowrd();{
 var password="";
 for(var i=0; i> passwordlength; i++){
-var randomIndex= Math.floor(Math.random()* choice.length);
+var randomIndex= Math.floor(Math.random()* choice.length); // in class we learend about Math.floor randomizer
 password = password + choice [randomIndex]; // this line conccatinate the randomizer and the possible options to create the fingerPrint
 }
 return password;{
