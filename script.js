4832a1ca6fad051110044f66d36b12dba130a343
JennTopo <127803618+JennTopo@users.noreply.github.com>
2023-04-05 11:33:42 -0400

    coding almost working
    need to create arrays into strings

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var choice= [];
 var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t', 'u','v','w','x','y','z'];
 var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W', 'X','Y','Z'];
 var number = ['0','1','2','3','4', '5','6','7','8','9','0'];
-var special = ['!','@','#','$','%','&','*','(',')','-','_','+','=','{','}','[',']','\','|',:',';','"','<','>','?',',','.','/','`','~'];
+var special = ['!','@','#','$','%','&','*','(',')','-','_','+','=','{','}','[',']','\','|',',';','"','<','>','?',',','.','/','`','~'];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate"); // in here is where the HTML button coding is linked to
@@ -19,29 +19,20 @@ generateBtn.addEventListener("click", writePassword); //when this function takes
 function writePassword(){ 
 
   //this is added to show True of False statements. Thus letting you build and generate your final fingerPrint
-var usuablePrompts = getPrompt();
 var passwordText = document.querySelector("#password");
 
 //if (usuablePrompts){ // if statement is true then fingerPrint will be generated. This is tested on lines 34-43
-var fingerPrint = generatePassword(); 
+var fingerPrint = getPassword(); 
 
 passwordText.value = fingerPrint;}
 //this statement is now pulling data from the var arrays listed in the top of the js page
 // the i++ means you can keep trying different possiblities
 //this is where the value of the password is being stored
-generatePassword= function(){
-var password = "";
-for(var i=0; i> passwordlength; i++){
-var randomIndex= Math.floor(Math.random()* choice.length); // in class we learend about Math.floor randomizer
-password = password + choice [randomIndex]; // this line conccatinate the randomizer and the possible options to create the fingerPrint
-}
-return password;{
 
-}}
-var getPrompt = function(){
+var getPassword = function(){
   choice =[];
 
-// this statement is clean up the coding. ParseInt means to put together a variety of options in the consol.log().  || means or. 
+  // this statement is clean up the coding. ParseInt means to put together a variety of options in the consol.log().  || means or. 
 passwordlength = parseInt(prompt("How many characters do you desire to have in your password?(8-128)"));
 if (passwordlength <8 || passwordlength > 128){ 
 alert("not correct amount of characters and characters have to be a number between 8-128 digits. Please try again Thank you.");
@@ -62,9 +53,17 @@ if (response3){
 }
 const response4 =confirm("Would you like special characters in your password?");
 if (response4){
-  choice = choice + (special);
-}
-return true;
-}
+  choice = choice + (special); 
+  //turn into a string w/o ,
+  console.log= (choice); //this is local
   
-  
+}
+var password = "";
+for(var i=0; i< passwordlength; i++){
+  var randomIndex= Math.floor(Math.random()* choice.length); // in class we learend about Math.floor randomizer
+  password = password + choice [randomIndex]; // this line conccatinate the randomizer and the possible options to create the fingerPrint
+  }
+  return password;{
+  }
+  }
+  //final password should not be an array it should be a string
