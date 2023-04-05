372a51272b1ef6c02c18062fdbe278f15243cf09
JennTopo <127803618+JennTopo@users.noreply.github.com>
2023-04-04 20:17:10 -0400

    new coding
    changed if statements at the end and fixed spelling errors

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var choice= [];
 var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t', 'u','v','w','x','y','z'];
 var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W', 'X','Y','Z'];
 var number = ['0','1','2','3','4', '5','6','7','8','9','0'];
-var speical = ['!','@','#','$','%','&','*','(',')','-','_','+','=','{','}','[',']','\','|',:',';','"','<','>','?',',','.','/','`','~'];
+var special = ['!','@','#','$','%','&','*','(',')','-','_','+','=','{','}','[',']','\','|',:',';','"','<','>','?',',','.','/','`','~'];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate"); // in here is where the HTML button coding is linked to
@@ -19,23 +19,18 @@ generateBtn.addEventListener("click", writePassword); //when this function takes
 function writePassword(){ 
 
   //this is added to show True of False statements. Thus letting you build and generate your final fingerPrint
-var usuablePrompts = getPrompts();
+var usuablePrompts = getPrompt();
 var passwordText = document.querySelector("#password");
 
-if (usuablePrompts){ // if statement is true then fingerPrint will be generated. This is tested on lines 34-43
-var fingerPrint = generatePassword();
-passwordText.value = fingerPrint;
-}
-else {
-  passwordText.value ="";
-}
-}
+//if (usuablePrompts){ // if statement is true then fingerPrint will be generated. This is tested on lines 34-43
+var fingerPrint = generatePassword(); 
 
+passwordText.value = fingerPrint;}
 //this statement is now pulling data from the var arrays listed in the top of the js page
-// the i++ means you cna keep trying different possiblities
+// the i++ means you can keep trying different possiblities
 //this is where the value of the password is being stored
-generatePassowrd();{
-var password="";
+generatePassword= function(){
+var password = "";
 for(var i=0; i> passwordlength; i++){
 var randomIndex= Math.floor(Math.random()* choice.length); // in class we learend about Math.floor randomizer
 password = password + choice [randomIndex]; // this line conccatinate the randomizer and the possible options to create the fingerPrint
@@ -43,30 +38,33 @@ password = password + choice [randomIndex]; // this line conccatinate the random
 return password;{
 
 }}
-getPrompt();{
+var getPrompt = function(){
   choice =[];
-}
-// this statement is clean up the coding. ParseInt means to put together a variety of options in the consol.log().  NaN means not a number, || means or. 
-characterlength = parseInt(prompt("How many characters do you desire to have in your password?(8-128"));
-if (isNaN(passwordlenght) || passwordlength <8 || paswordlength > 128){ 
+
+// this statement is clean up the coding. ParseInt means to put together a variety of options in the consol.log().  || means or. 
+passwordlength = parseInt(prompt("How many characters do you desire to have in your password?(8-128)"));
+if (passwordlength <8 || passwordlength > 128){ 
 alert("not correct amount of characters and characters have to be a number between 8-128 digits. Please try again Thank you.");
 return false;
 }
 
-if (confirm("Would you like lowercase letters in your password?")){
+const response =confirm("Would you like lowercase letters in your password?");
+if (response){
 choice = choice + (lowercase);
 }
-
-if (confirm("Would you like uppercasee letters in your password?")){
+const response2 =confirm("Would you like uppercase letters in your password?");
+if (response2){
   choice = choice + (uppercase);
 }
-  if (confirm("Would you like number letters in your password?")){
-    choice = choice + (number);
-  }
-    if (confirm("Would you like special letters in your password?")){
-      choice = choice + (speical);
-    }
-return true;{
-
+const response3 =confirm("Would you like numbers in your password?");
+if (response3){
+  choice = choice + (number);
 }
+const response4 =confirm("Would you like special characters in your password?");
+if (response4){
+  choice = choice + (special);
+}
+return true;
+}
+  
   
