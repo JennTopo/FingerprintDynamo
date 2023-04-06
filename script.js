364a6f455c44ca46ea24d76b01278620f7da6816
JennTopo <127803618+JennTopo@users.noreply.github.com>
2023-04-05 22:03:15 -0400

    working code
    had to concat the array and now the code works

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ as well as the possible choices in letters, digits and special characters. */
 var passwordlength = (i=0)[i>8 & i < 128];
 var characterlength= 8;
 var choice= [];
+
 var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t', 'u','v','w','x','y','z'];
 var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W', 'X','Y','Z'];
 var number = ['0','1','2','3','4', '5','6','7','8','9','0'];
-var special = ['!','@','#','$','%','&','*','(',')','-','_','+','=','{','}','[',']','\','|',',';','"','<','>','?',',','.','/','`','~'];
+var special =['!','@','#','$','%','&','*','(',')','-','_','+','=','{','}','[',']','\','|',',';','"','<','>','?',',','.','/','`','~'];
+
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate"); // in here is where the HTML button coding is linked to
@@ -41,29 +43,29 @@ return false;
 
 const response =confirm("Would you like lowercase letters in your password?");
 if (response){
-choice = choice + (lowercase);
+choice = choice.concat(lowercase);
 }
 const response2 =confirm("Would you like uppercase letters in your password?");
 if (response2){
-  choice = choice + (uppercase);
+  choice = choice.concat(uppercase);
 }
 const response3 =confirm("Would you like numbers in your password?");
 if (response3){
-  choice = choice + (number);
+  choice = choice.concat(number);
 }
 const response4 =confirm("Would you like special characters in your password?");
 if (response4){
-  choice = choice + (special); 
-  //turn into a string w/o ,
-  console.log= (choice); //this is local
-  
+  choice = choice.concat(special); 
+ 
 }
 var password = "";
 for(var i=0; i< passwordlength; i++){
   var randomIndex= Math.floor(Math.random()* choice.length); // in class we learend about Math.floor randomizer
+  console.log(choice[randomIndex]);
+  console.log(password);
   password = password + choice [randomIndex]; // this line conccatinate the randomizer and the possible options to create the fingerPrint
   }
   return password;{
   }
   }
-  //final password should not be an array it should be a string
+  //final password should not be an array it should be a string there should be no commas.
